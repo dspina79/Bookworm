f8fe1d040751c9ad6490bd818c45f3ab87be0839
Dave Spina <dspina79@gmail.com>
2021-01-01 21:55:33 -0500

    Added new book add sheet view and rating view

---
+++ Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,63 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Dave Spina on 1/1/21.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var genre = ""
+    @State private var rating = 3
+    @State private var review = ""
+    
+    private var genres = ["Fantasy", "Horror", "Kids", "Thriller", "Nonfiction", "Poetry", "Romance", "Sci-fi", "Other"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Title", text: $title)
+                    TextField("Author", text: $author)
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                Section {
+                    RatingView(rating: $rating, label: "Rating")
+                    TextField("Review", text: $review)
+                }
+                
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: self.moc)
+                        newBook.id = UUID()
+                        newBook.title = self.title
+                        newBook.author = self.author
+                        newBook.review = self.review
+                        newBook.rating = Int16(self.rating)
+                        newBook.genre = self.genre
+                        
+                        try? moc.save()
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                }
+            }
+            .navigationBarTitle("Add Book")
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
---
+++ Bookworm/Bookworm/RatingView.swift
@@ -0,0 +1,50 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Dave Spina on 1/1/21.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var label = ""
+    var maximumRating = 5
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    var body: some View {
+        HStack {
+            if !label.isEmpty {
+                Text(label)
+            }
+            
+            ForEach(1..<maximumRating + 1) { num in
+                image(for: num)
+                    .foregroundColor(num > rating ? offColor : onColor)
+                    .onTapGesture {
+                        self.rating = num
+                    }
+            }
+        }
+    }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView(rating: .constant(4))
+    }
+}
